b739b45882820520baa82651ffa3aaf73a2dc49c
proangel1215 <proangel1215@gmail.com>
2023-11-24 12:23:44 +0100

    display register form and insert user in db

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, redirect
+from .forms import RegisterForm
+from .models.User import User
+from . import db
 
 auth = Blueprint("auth", __name__)
 
@@ -6,3 +9,30 @@ auth = Blueprint("auth", __name__)
 @auth.route("/", methods=["GET", "POST"])
 def login():
     return "login"
+
+
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            email = request.form.get("email")
+            pseudo = request.form.get("pseudo")
+            password = request.form.get("password")
+            password_confirmation = request.form.get("password_confirmation")
+
+            if password != password_confirmation:
+                flash("Les 2 mots de passe ne correspondent pas !", category="error")
+            
+            user = User(username=pseudo, email=email, password_plaintext=password)
+            db.session.add(user)
+            db.session.commit()
+            flash("Inscription !")
+            
+            return redirect("/")
+
+    return render_template("register.html", form=form)
+
---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    TextAreaField,
+    PasswordField,
+    SelectField,
+    IntegerField,
+    EmailField,
+)
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    pseudo = StringField("pseudo", validators=[DataRequired()])
+    email = EmailField("email", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    password_confirmation = PasswordField(
+        "password_confirmation", validators=[DataRequired()]
+    )
