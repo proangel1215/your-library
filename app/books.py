c9a1b7e6797195f4d2a7c8e3ff8a00835185bd4b
proangel1215 <proangel1215@gmail.com>
2023-11-27 13:39:09 +0100

    start to display books google api results

--- app/books.py
+++ app/books.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 from .forms import SearchBookForm
+import requests
+import json 
 
 books = Blueprint("books", __name__)
 
@@ -10,4 +12,29 @@ books = Blueprint("books", __name__)
 def home():
     form = SearchBookForm()
 
-    return render_template("books/home.html", form=form)
+    if request.method == "POST" and form.validate_on_submit():
+
+        search_str = form.search.data
+
+        response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={search_str}')
+
+        books_results = []
+
+        if response.status_code == 200:
+            results = response.json()['items']
+            for result in results:
+                print(result['volumeInfo']['authors'])
+                # books_results.append({
+                #     "id": result['id'],
+                #     "title": result['volumeInfo']['title'],
+                #     "author": result['volumeInfo']['authors'][0],
+                #     # "description": result['description'],
+                #     "publishedDate": result['volumeInfo']['publishedDate'],
+                # })
+
+            # print(results)
+
+
+    
+
+    return render_template("books/home.html", form=form, books_results=results)
